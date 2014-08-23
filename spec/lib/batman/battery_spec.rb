fc07debdca479690ba4c5c4b0f608c5d1ac2d284
Joakim Reinert <mail@jreinert.com>
2014-08-23 14:57:10 +0200

    Added specs for Battery

---
+++ spec/lib/batman/battery_spec.rb
@@ -0,0 +1,43 @@
+require 'batman/battery'
+module Batman
+  describe Battery do
+
+    describe '.new' do
+
+      it 'throws an AbstractError if directly instantiated' do
+        expect { Battery.new }.to raise_error AbstractError
+      end
+
+      let(:subclass) { Class.new(Battery) }
+
+      it 'allows instantiation of subclasses' do
+        subclass.new
+      end
+
+      it 'accepts an index and sets the corresponding instance variable' do
+        battery = subclass.new(1)
+
+        expect(battery.instance_variable_get(:@index)).to eq 1
+      end
+
+      it 'sets index to 0 by default' do
+        battery = subclass.new
+
+        expect(battery.instance_variable_get(:@index)).to eq 0
+      end
+
+    end
+
+    let(:battery) { Class.new(Battery).new }
+
+    [:remaining_percent, :power, :remaining_time].each do |method|
+
+      describe "##{method}" do
+
+        it 'throws an NotImplementedError' do
+          expect { battery.send(method) }.to raise_error NotImplementedError
+        end
+      end
+    end
+  end
+end

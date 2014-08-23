046473f318bd4223bbbdd551e4ddef65c8f561b1
Joakim Reinert <mail@jreinert.com>
2014-08-23 13:54:49 +0200

    Added abstract battery class

---
+++ lib/batman/battery.rb
@@ -0,0 +1,24 @@
+require 'errors'
+
+module Batman
+  class Battery
+
+    def initialize
+      if self.class == Battery
+        raise AbstractError.new('cannot instantiate Battery')
+      end
+    end
+
+    def remaining_percent
+      raise NotImplementedError.new
+    end
+
+    def power
+      raise NotImplementedError.new
+    end
+
+    def remaining_time
+      raise NotImplementedError.new
+    end
+  end
+end
---
+++ lib/batman/errors.rb
@@ -0,0 +1,4 @@
+module Batman
+  class AbstractError < RuntimeError; end
+  class NotImplementedError < RuntimeError; end
+end

3526700ec8f4368f1e1439df845ff8e9e4519f75
Joakim Reinert <mail@jreinert.com>
2014-08-25 11:11:45 +0200

    Added an example config

---
+++ battman_example.rb
@@ -0,0 +1,57 @@
+require 'battman'
+
+Battman::Battman.new do |battman|
+
+  battman.watch :acpi do |watcher|
+
+    current_state = :discharging # to be on the safe side
+
+    watcher.every 2.seconds do |status|
+
+      status.check(:state) do |value, last_value|
+        current_state = value
+        state_changed = value != last_value
+
+        if state_changed
+          `notify-send 'Battery is #{current_state}'`
+        end
+      end
+
+    end
+
+    watcher.every 60.seconds do |status|
+
+      suspend = false
+
+      status.check(:remaining_percent) do |value|
+
+        if current_state == :discharging && value.in?(0...5)
+          `i3lock && systemctl suspend` if suspend
+          `notify-send -u critical 'Battery is critical'`
+
+          if value.in?(0..2)
+            `notify-send -u critical 'Suspending in 60 seconds! (plug in cable to abort)'`
+            suspend = true
+          end
+        else
+          supend = false
+        end
+
+      end
+
+    end
+
+    watcher.every 5.minutes do |status|
+
+      status.check(:remaining_percent) do |value|
+        if current_state == :discharging && value.in?(5...10)
+          `notify-send -u normal 'Battery is low'`
+        end
+      end
+
+    end
+
+  end
+
+  battman.run
+end
